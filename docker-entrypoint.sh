cc8897d61fc0d81939257da783bd35c6fb559d13
antontackapp <anton@tackapp.net>
2022-08-03 11:34:37 +0300

    load fixture on startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup --"
-    # python3 manage.py loaddata data45.json
+    python3 manage.py loaddata fixture1.json
 else
     echo "-- Not first container startup --"
 fi
