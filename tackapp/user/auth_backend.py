0d32f3557be2a3f96f3d3fbfc674097711cfd54a
antontackapp <anton@tackapp.net>
2022-08-23 14:48:21 +0300

    email lowercase fix

--- tackapp/user/auth_backend.py
+++ tackapp/user/auth_backend.py
@@ -1,42 +1,4 @@
-# from .models import User
-# from django.db.models import Q
-#
-#
-# class AuthBackend(object):
-#     supports_object_permissions = True
-#     supports_anonymous_user = False
-#     supports_inactive_user = False
-#
-#     def get_user(self, user_id):
-#         try:
-#             return User.objects.get(pk=user_id)
-#         except User.DoesNotExist:
-#             return None
-#
-#     def authenticate(self, phone_number, password):
-#         try:
-#             user = User.objects.get(
-#                 Q(username=phone_number) | Q(email=phone_number) | Q(phone_number=phone_number)
-#             )
-#         except User.DoesNotExist:
-#             return None
-#
-#         return user if user.check_password(password) else None
 from django.db.models import Q
-from rest_framework_simplejwt.serializers import TokenObtainSerializer
-
-from django.contrib.auth import authenticate, get_user_model
-# from django.utils.translation import ugettext as _
-# from rest_framework import serializers
-#
-# from rest_framework_jwt.settings import api_settings
-
-
-# User = get_user_model()
-# jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-# jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-# jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
-# jwt_get_username_from_payload = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -51,6 +13,9 @@ class CustomJWTSerializer(TokenObtainPairSerializer):
             'phone_number': attrs.get("phone_number"),
             'password': attrs.get("password")
         }
+
+        if type(credentials['phone_number']) is str:
+            credentials['phone_number'] = credentials['phone_number'].lower()
 
         try:
             user = User.objects.get(
