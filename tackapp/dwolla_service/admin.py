3ee9e58b783ee48dfcda47e1140c9673cb6fca09
antontackapp <anton@tackapp.net>
2022-08-24 09:49:03 +0300

    DwollaEvent db model

--- tackapp/dwolla_service/admin.py
+++ tackapp/dwolla_service/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import ModelAdmin
+
+from dwolla_service.models import DwollaEvent
+
+
+@admin.register(DwollaEvent)
+class DwollaEventAdmin(ModelAdmin):
+    list_display = ('event_id', 'topic', 'timestamp')
