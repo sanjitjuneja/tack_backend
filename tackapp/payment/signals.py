b29e9ac544fc170a8d9b17109c562c54a9771cbf
antontackapp <anton@tackapp.net>
2022-09-01 12:42:56 +0300

    logging create_pm_holder

--- tackapp/payment/signals.py
+++ tackapp/payment/signals.py
@@ -16,6 +16,7 @@ def add_balance_to_user(instance: PaymentIntent, created: bool, *args, **kwargs)
 
 @receiver(signal=post_save, sender=PaymentMethod)
 def create_pm_holder(instance: PaymentMethod, created: bool, *args, **kwargs):
+    logging.getLogger().warning(f"{kwargs = }")
     if created:
         spmh = StripePaymentMethodsHolder.objects.create(stripe_pm=instance)
         logging.getLogger().warning(f"{spmh = }")
