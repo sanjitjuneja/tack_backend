3281fc8b2458fef731159211ed501c41d499c9b5
antontackapp <anton@tackapp.net>
2022-09-01 12:53:58 +0300

    changed signals PaymentIntent, PaymentMethod

--- tackapp/payment/signals.py
+++ tackapp/payment/signals.py
@@ -3,18 +3,19 @@ import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from djstripe.models import PaymentIntent, PaymentMethod
+from djstripe.signals import WEBHOOK_SIGNALS
 
 from payment.models import StripePaymentMethodsHolder
 from payment.services import add_money_to_bank_account
 
 
-@receiver(signal=post_save, sender=PaymentIntent)
+@receiver(signal="payment_intent.succeeded", sender=PaymentIntent)
 def add_balance_to_user(instance: PaymentIntent, created: bool, *args, **kwargs):
     if instance.status == "succeeded":
         add_money_to_bank_account(instance.amount)
 
 
-@receiver(signal=post_save, sender=PaymentMethod)
+@receiver(signal="payment_method.attached", sender=PaymentMethod)
 def create_pm_holder(instance: PaymentMethod, created: bool, *args, **kwargs):
     logging.getLogger().warning(f"{kwargs = }")
     if created:
