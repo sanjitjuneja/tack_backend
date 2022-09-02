9dac2b7af8916222241ca34d8027a0e3a6ba1967
antontackapp <anton@tackapp.net>
2022-09-02 10:48:00 +0300

    dwolla webhook

--- tackapp/payment/dwolla_service.py
+++ tackapp/payment/dwolla_service.py
@@ -1,5 +1,4 @@
 import logging
-from pprint import pprint
 
 import dwollav2
 
@@ -21,16 +20,16 @@ dwolla_client = dwollav2.Client(
     on_grant=lambda t: save_dwolla_token(t)
 )
 
-# webhook_request = {
-#   'url': 'http://44.203.217.242:8020/api/v1/webhooks/dwolla/',
-#   'secret': DWOLLA_WEBHOOK_SECRET
-# }
+webhook_request = {
+  'url': 'http://44.203.217.242/api/v1/webhooks/dwolla/',
+  'secret': DWOLLA_WEBHOOK_SECRET
+}
 # webhook_request = {
 #   'url': 'https://zgqcy9sg1g.execute-api.us-east-1.amazonaws.com/DwollaTest',
 #   'secret': DWOLLA_WEBHOOK_SECRET
 # }
 
-# token = dwolla_client.Auth.client()
+token = dwolla_client.Auth.client()
 
 
 # token.delete("webhook-subscriptions/f666b8f9-9800-4f64-bcc8-a1ceceb7f061")
@@ -38,7 +37,8 @@ dwolla_client = dwollav2.Client(
 # print(subscriptions)
 # pprint(subscriptions.body)
 # print(subscriptions.headers)
-# subscription = token.post('webhook-subscriptions', webhook_request)
+subscription = token.post('webhook-subscriptions', webhook_request)
+logging.getLogger().warning(f"{subscription = }")
 # logging.getLogger().warning(subscription)
 # print(subscription)
 # print(subscription.body)
