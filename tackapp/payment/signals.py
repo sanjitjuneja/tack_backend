468f4199ac4a00c17276c7d0bb8b28f7fbc7d7fc
antontackapp <anton@tackapp.net>
2022-09-02 10:41:48 +0300

    Added/changed @webhooks.handler for payment_intent.succeeded, payment_method.attached

--- tackapp/payment/signals.py
+++ tackapp/payment/signals.py
@@ -1,55 +1,20 @@
-import logging
+from djstripe.enums import PaymentIntentStatus
 
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-import djstripe.models
 from djstripe import webhooks
-
 from djstripe.models import PaymentIntent, PaymentMethod
-from djstripe.signals import WEBHOOK_SIGNALS
 
 from payment.models import StripePaymentMethodsHolder
 from payment.services import add_money_to_bank_account
 
 
-@receiver(signal=post_save, sender=PaymentIntent)
-def add_balance_to_user(instance: PaymentIntent, created: bool, *args, **kwargs):
-    if instance.status == "succeeded":
-        add_money_to_bank_account(instance.amount)
+@webhooks.handler("payment_intent.succeeded")
+def add_balance_to_user(event, *args, **kwargs):
+    pi = PaymentIntent.objects.get(id=event.data.get("object").get("id"))
+    if pi.status != PaymentIntentStatus.succeeded:
+        add_money_to_bank_account(pi.amount)
 
-
-from djstripe import webhooks
 
 @webhooks.handler("payment_method.attached")
-def my_handler(event, *args, **kwargs):
-    logging.getLogger().warning(f"{args = }")
-    logging.getLogger().warning(f"{kwargs = }")
-    logging.getLogger().warning(f"{event = }")
-    logging.getLogger().warning(f"{event.type = }")
-    logging.getLogger().warning(f"{event.data = }")
-    instance = djstripe.models.PaymentMethod.objects.get(id=event.data.get("object").get("id"))
-    logging.getLogger().warning(f"{instance = }")
-    spmh = StripePaymentMethodsHolder.objects.create(stripe_pm=instance)
-    logging.getLogger().warning(f"{spmh = }")
-
-# @receiver(signal=WEBHOOK_SIGNALS.get("payment_method.attached"))
-# def create_pm_holder(*args, **kwargs):
-#
-#     evt = djstripe.models.Event.objects.get(id=kwargs.get("id"))
-#     logging.getLogger().warning(f"{evt = }")
-#     logging.getLogger().warning(f"{evt.data = }")
-#     instance = djstripe.models.PaymentMethod.objects.get(id=evt.data.get("object").get("id"))
-#     logging.getLogger().warning(f"{instance = }")
-#     spmh = StripePaymentMethodsHolder.objects.create(stripe_pm=instance)
-#     logging.getLogger().warning(f"{spmh = }")
-
-
-# @receiver(signal=post_save, sender=PaymentMethod)
-# def create_pm_holder(instance: PaymentMethod, created: bool, *args, **kwargs):
-#     logging.getLogger().warning(f"{args = }")
-#     logging.getLogger().warning(f"{kwargs = }")
-#     logging.getLogger().warning(f"{instance = }")
-#     if created:
-#         spmh = StripePaymentMethodsHolder.objects.create(stripe_pm=instance)
-#         logging.getLogger().warning(f"{spmh = }")
+def create_pm_holder(event, *args, **kwargs):
+    instance = PaymentMethod.objects.get(id=event.data.get("object").get("id"))
+    StripePaymentMethodsHolder.objects.create(stripe_pm=instance)
