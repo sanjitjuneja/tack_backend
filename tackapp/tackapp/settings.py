5bda1809c5c9938a210e1a3683502e6963062e86
antontackapp <anton@tackapp.net>
2022-09-07 15:21:39 +0300

    CSRF_TRUSTED_ORIGINS

--- tackapp/tackapp/settings.py
+++ tackapp/tackapp/settings.py
@@ -309,10 +309,10 @@ SIMPLE_JWT = {
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8020",
-    "http://44.203.217.242:8020",
+    "http://54.158.0.200:8020",
     "https://backend.tackapp.net",
     "http://172.31.8.161",
-    "http://44.203.217.242",
+    "http://54.158.0.200",
 ]
 
 STRIPE_PUBLISHABLE_KEY = env("STRIPE_PUBLISHABLE_KEY")
