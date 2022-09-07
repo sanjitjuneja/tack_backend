f11d0b17c4679fef8055390356e04e2859de2b52
antontackapp <anton@tackapp.net>
2022-09-07 15:56:08 +0300

    ASGI router

--- tackapp/tackapp/asgi.py
+++ tackapp/tackapp/asgi.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from tackapp.urls import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tackapp.settings')
 # Initialize Django ASGI application early to ensure the AppRegistry
@@ -19,5 +21,11 @@ django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket":  # AllowedHostsOriginValidator(
+        # AuthMiddlewareStack(
+            URLRouter(
+                websocket_urlpatterns
+            )
+        # )
+    # ),
 })
