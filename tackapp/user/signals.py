d505d1bc7e3ea58e635f2c5e87e423b6bf45ceb9
antontackapp <anton@tackapp.net>
2022-09-07 15:26:37 +0300

    Dwolla and Stripe turn off

--- tackapp/user/signals.py
+++ tackapp/user/signals.py
@@ -9,13 +9,13 @@ from user.services import create_api_accounts, deactivate_dwolla_customer, delet
 @receiver(signal=post_save, sender=User)
 def create_stripe_dwolla_account(instance: User, created: bool, *args, **kwargs):
     if created and not instance.is_superuser:
-        stripe_id, dwolla_id = create_api_accounts(instance)
+        # stripe_id, dwolla_id = create_api_accounts(instance)
 
         # Creating record in our system
         BankAccount.objects.create(
             user=instance,
-            stripe_user=stripe_id,
-            dwolla_user=dwolla_id
+            # stripe_user=stripe_id,
+            # dwolla_user=dwolla_id
         )
 
 
