bbc72f7bcb85209da729703ee3f6ffa8f6914184
antontackapp <anton@tackapp.net>
2022-09-13 21:23:36 +0300

    Log GOOGLE_APPLICATION_CREDENTIALS

--- tackapp/tackapp/settings.py
+++ tackapp/tackapp/settings.py
@@ -318,6 +318,7 @@ FCM_DJANGO_SETTINGS = {
 }
 
 GOOGLE_APPLICATION_CREDENTIALS = os.path.join(BASE_DIR, 'tack-technologies-firebase-adminsdk-hq5db-8881ca10c9.json')
+logging.getLogger().warning(f"{GOOGLE_APPLICATION_CREDENTIALS = }")
 
 S3_BUCKET_TACKAPPSTORAGE = AWS_S3_CUSTOM_DOMAIN
 
