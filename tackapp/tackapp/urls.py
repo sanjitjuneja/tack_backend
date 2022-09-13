94b16cb8538847ef10857972740ef3da36e75279
antontackapp <anton@tackapp.net>
2022-09-13 11:40:35 +0300

    Some deploy fixes

--- tackapp/tackapp/urls.py
+++ tackapp/tackapp/urls.py
@@ -3,18 +3,15 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 import debug_toolbar
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView, TokenBlacklistView
 
 from tackapp import consumers
 from user.auth_backend import CustomJWTSerializer
 
 urlpatterns = [
-    # path('jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    # path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),  # Django JET dashboard URLS
     re_path(r"admin/", admin.site.urls),
     re_path(r"api/v1/", include("tack.urls")),
-    # re_path(r"api/v1/", include("socials.urls")),
     re_path(r"api/v1/", include("user.urls")),
     re_path(r"api/v1/", include("group.urls")),
     re_path(r"api/v1/", include("review.urls")),
@@ -24,13 +21,9 @@ urlpatterns = [
     re_path(r"api/v1/tokens/verify/", TokenVerifyView.as_view(), name='token_verify'),
     re_path(r"api/v1/tokens/blacklist/", TokenBlacklistView.as_view(), name='token_blacklist'),
     path("stripe/", include("djstripe.urls", namespace="djstripe")),
-    # re_path(r"", include("social_django.urls", namespace="social")),
 
-    path("__debug__/", include(debug_toolbar.urls)),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     # Optional UI:
     path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 websocket_urlpatterns = [
@@ -40,3 +33,4 @@ websocket_urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
