d7893570c96adc0878f8ffb881710973c81e4ffd
antontackapp <anton@tackapp.net>
2022-09-26 11:28:46 +0300

    change github aws credentionals

--- tackapp/tackapp/urls.py
+++ tackapp/tackapp/urls.py
@@ -42,4 +42,3 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += path("__debug__/", include(debug_toolbar.urls)),
-
