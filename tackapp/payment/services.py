64a5728652da990c37ce0cf762011bd9f38e0689
antontackapp <anton@tackapp.net>
2022-09-30 22:02:40 +0300

    Plaid Product changed from transactions to auth

--- tackapp/payment/services.py
+++ tackapp/payment/services.py
@@ -68,8 +68,8 @@ def get_link_token(dwolla_id_user):
     """Get link token for FE to initiate plaid authentication"""
     logger.debug("INSIDE get_link_token")
     request = LinkTokenCreateRequest(
-        products=[Products('transactions')],  # , Products("identity_verification")],
-        client_name="Plaid Test App",
+        products=[Products('auth')],  # , Products("identity_verification")],
+        client_name="Tack App",
         country_codes=[CountryCode('US')],
         language='en',
         # webhook='https://sample-webhook-uri.com',
