10d5af87b4c034e25e831f46d06e14611d5cd194
antontackapp <anton@tackapp.net>
2022-10-03 15:01:16 +0300

    calculate_fee_difference_for_transaction_model

--- tackapp/payment/tasks.py
+++ tackapp/payment/tasks.py
@@ -6,7 +6,7 @@ from payment.models import Transaction
 
 @shared_task
 def calculate_fee_difference_for_transaction_model():
-    transactions = Transaction.objects.filter(service_fee=None)
+    transactions = Transaction.objects.all()
     transactions.update(
         fee_difference=F('amount_with_fees') - F('amount_requested') - F('service_fee')
     )
